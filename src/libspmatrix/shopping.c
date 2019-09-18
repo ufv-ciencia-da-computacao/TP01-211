c502f7e49b17c38c73e8c14971558e689d85da79
Dener Vieira <deneribeiro10@hotmail.com>
2019-09-18 10:33:49 -0300

    shopping verification for valid data

--- src/libspmatrix/shopping.c
+++ src/libspmatrix/shopping.c
@@ -1,8 +1,14 @@
 #include "../include/shopping.h"
 
+static int dateToDayOfYear(int *dayOfYear, int day, int month, int year);
+static int dayOfYearToDate(int dayOfYear, int *day, int *month, int year);
+static int verifyDate(int day, int month, int year);
+static int isLeapYear(int year);
+static int verifyQttProducts(int qttProducts);
+
 static int dateToDayOfYear(int *dayOfYear, int day, int month, int year) {
   int daysInMonth[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-  if(year % 4 == 0 && year % 100 != 0 || year % 400 == 0) daysInMonth[1] = 29;
+  if(isLeapYear(year)) daysInMonth[1] = 29;
 
   int i;
   int daysPassed = 0;
@@ -15,7 +21,7 @@ static int dateToDayOfYear(int *dayOfYear, int day, int month, int year) {
 
 static int dayOfYearToDate(int dayOfYear, int *day, int *month, int year) {
   int daysInMonth[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-  if(year % 4 == 0 && year % 100 != 0 || year % 400 == 0) daysInMonth[1] = 29;
+  if(isLeapYear(year)) daysInMonth[1] = 29;
 
   int i;
   int totalDays = 0;
@@ -29,10 +35,29 @@ static int dayOfYearToDate(int dayOfYear, int *day, int *month, int year) {
   }
 
   return 0;
+}
 
+static int verifyDate(int day, int month, int year) {
+  int daysInMonth[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+  if(isLeapYear(year)) daysInMonth[1] = 29;
+  if(!(0 < year && year <= 9999)) return 0;
+  if(!(1 <= month && month <= 12)) return 0;
+  if(!(1 <= day && day <= daysInMonth[month-1])) return 0;
+  return 1;
+}
+
+static int isLeapYear(int year) {
+  return (year % 4 == 0 && year % 100 != 0 || year % 400 == 0);
+}
+
+static int verifyQttProducts(int qttProducts) {
+  return (qttProducts > 0);
 }
 
 int shoppingInit(Shopping *shop, int day, int month,  int year, int qttProducts) {
+  if(!verifyDate(day, month, year) || !verifyQttProducts(qttProducts)) {
+    return 1;
+  }
   dateToDayOfYear(&(shop->dayOfYear), day, month-1, year);
   shop->year = year;
   shop->qttProducts = qttProducts;
--- src/tests/shopping/shopping_main.c
+++ src/tests/shopping/shopping_main.c
@@ -4,11 +4,14 @@
 
 int main() {
   Shopping shop;
-  int day, month;
-  scanf("%d %d", &day, &month);
-  shoppingInit(&shop, day, month, 2020, 100);
-  char str[128];
-  shoppingToString(shop, str);
-  printf("%s", str);
+  int day, month, year, qttProducts, error;
+  printf("(dia) (mes) (ano) (quantidade de produtos)\n");
+  scanf("%d %d %d %d", &day, &month, &year, &qttProducts);
+  error = shoppingInit(&shop, day, month, year, qttProducts);
+  if(!error) {
+    char str[128];
+    shoppingToString(shop, str);
+    printf("%s", str);
+  } 
   return 0;
 }
